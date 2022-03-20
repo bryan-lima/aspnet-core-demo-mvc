0f1b3456dbbd37ad247a395d6b4ff5209b495839
Bryan <bryanlima200@outlook.com>
2022-03-20 14:39:28 -0300

    Removido configurações do módulo anterior

--- AspNetCore.DemoMVC/Controllers/HomeController.cs
+++ AspNetCore.DemoMVC/Controllers/HomeController.cs
@@ -8,32 +8,19 @@ using System.Threading.Tasks;
 
 namespace AspNetCore.DemoMVC.Controllers
 {
-    [Route("")]
-    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
-        [Route("")]
-        [Route("pagina-inicial")]
-        [Route("pagina-inicial/{id:int}/{categoria?}")]
         public IActionResult Index(int id, string categoria)
         {
             return View();
         }
 
-        [Route("privacidade")]
-        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
-            //return Json("{'nome': 'Bryan'}");
 
-            //var fileBytes = System.IO.File.ReadAllBytes(@".\wwwroot\arquivo.txt");
-            //var fileName = "ola.txt";
-            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
-
-            return Content("Qualquer coisa");
+            return View();
         }
 
-        [Route("erro-encontrado")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
