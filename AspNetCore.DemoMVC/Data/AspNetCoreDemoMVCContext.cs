994f861d155180731df5c5cb22c0b68e7525e2e7
Bryan <bryanlima200@outlook.com>
2022-03-23 23:13:56 -0300

    Gerado controller Filmes, views pertencentes à Filmes, e AspNetCoreDemoMVCContext utilizando método scaffold

---
+++ AspNetCore.DemoMVC/Data/AspNetCoreDemoMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspNetCore.DemoMVC.Models;
+
+namespace AspNetCore.DemoMVC.Data
+{
+    public class AspNetCoreDemoMVCContext : DbContext
+    {
+        public AspNetCoreDemoMVCContext (DbContextOptions<AspNetCoreDemoMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspNetCore.DemoMVC.Models.Filme> Filme { get; set; }
+    }
+}
